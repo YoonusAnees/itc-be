5c9681ec8809c2fd5027175423ab586a73d4f6b8
yoonusanees <yoonusaneesniis@gmail.com>
2026-05-18 11:47:02 +0530

    feat(Corse)

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import quoteRoutes from "./routes/quote.routes.js";
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:8000",
+  origin: process.env.CLIENT_URL || "http://localhost:12000",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
